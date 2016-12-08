43d2bdefc68d00509ecd6e43f9b91cefe618f3dc
rohit.bhattacharjee99 <rohit.bhattacharjee99@gmail.com>
2016-12-08 01:45:18 -0600

    queries for retreiving data from mongoDB server

---
+++ rook/rookquery.R
@@ -0,0 +1,150 @@
+##
+##  querydata.R
+##
+##  8/4/15
+##
+library(jsonlite)
+library(RJSONIO)
+#sourc=NULL
+query.app<-function(env){
+  
+  
+  production<-FALSE     ## Toggle:  TRUE - Production, FALSE - Local Development
+  warning<-FALSE
+  
+  if(production){
+    sink(file = stderr(), type = "output")
+  }
+  request <- Request$new(env)
+  response <- Response$new(headers = list( "Access-Control-Allow-Origin"="*"))
+  result <- list()
+  print(request$POST()$solaJSON)
+  valid <- jsonlite::validate(request$POST()$solaJSON)
+  #print(valid)
+  #print(1)
+  if(!valid) {
+    warning <- TRUE
+    result <- list(warning="The request is not valid json. Check for special characters.")
+  }
+  #print(2)
+  
+# 
+  #url="http://10.176.148.60:5000/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&select=source&query={}"
+  #url2="http://10.176.148.60:5000/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&select=date,location,country&query={}"
+  
+  
+  #url="http://10.176.148.60:5000/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query=<query-for-data>&select=<comma-separated-list-of-attributes>"
+# &select=date,country
+  #json='[{\"location\":{\"$in\":[\"Budapest\",\"Abuja\"]}}]'
+  #json2=substr(json,2,(nchar(json)-1))
+  
+  #json3=gsub("\\+","",json2)
+  #valid<-jsonlite::validate(json);
+#  everything <- rjson::fromJSON(json)
+  
+  #print(3)  
+  if(!warning) {
+    everything <- request$POST()$solaJSON
+    print("value of Everything")
+    print(everything)
+  }
+  
+  #################################################################
+  #################################################################
+  ##################################################################
+  temp1=substr(everything,2,(nchar(everything)-1))
+  temp2=rjson::fromJSON(temp1)
+  print("temp")
+  print(temp2)
+  type=temp2$type
+  if(type=="prequery"){
+  
+    #testurl='http://10.176.148.60:5002/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query={}&unique=country_code'
+    preurl=c("http://10.176.148.60:5002/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query={}&unique=src_actor",
+             "http://10.176.148.60:5002/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query={}&unique=tgt_actor",
+             "http://10.176.148.60:5002/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query={}&unique=date8",
+             "http://10.176.148.60:5002/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query={}&unique=country_code")
+    
+    #preurl2='http://10.176.148.60:5000/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query={"code":"057","code":"010"}'
+
+    
+    #srcagnturl='http://10.176.148.60:5002/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query={"src_actor":{"$in":["USA","DEU"]}}&select=src_actor,src_agent'
+    #preurl="http://10.176.148.60:5000/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&select=source,target,date,location&query={}"
+    #predata=jsonlite::fromJSON(preurl)
+    #datatest=jsonlite::fromJSON(srcagnturl)
+    #test=predata$data
+    #testsrc=datatest$data
+  
+
+  
+  
+  
+ 
+  print("test2")
+  
+  sourc<<-(jsonlite::fromJSON(preurl[1]))$data
+  target<<-(jsonlite::fromJSON(preurl[2]))$data
+  #date=(jsonlite::fromJSON(preurl[1]))$data
+  #location=predata$data$location
+  #country=mydata$data$country
+  
+  location=(jsonlite::fromJSON(preurl[4]))$data
+  date=jsonlite::fromJSON(preurl[3])$data
+  datetest=as.Date(date,"%Y%m%d")
+  datesort=sort(datetest)
+  #maxdate=datesort[which.max(datesort)]
+  maxdate=datesort[which.max(datesort)]
+  #mindate=datesort[which.min(datesort)]
+  mindate=datesort[which.min(datesort)]
+  
+  jsondata=jsonlite::toJSON(list(maxdate=maxdate,mindate=mindate,location=location,sourceactors=sourc,targetactors=target))
+  #print(jsondata)
+  result=list(jsondata)
+  }
+  #write(jsondata,file="pretarget.json")
+  #################################################################
+  #################################################################
+  ##################################################################
+  else{
+  #tmp1=substr(everything,2,(nchar(everything)-1))
+#  tmp2=rjson::fromJSON(everything)
+  query=temp2$query
+  print("value of query")
+  print(query)
+      json2=jsonlite::toJSON(query)
+    print("value of json2")
+    print(json2)
+   #json2='{"$or":[{"date":{"$gte":"$date(2016-05-24)","$lte":"$date(2016-05-25)"}},{"location":{"$in":["United States of America"]}}]}'
+   json3=gsub(" ","%20",json2) 
+   #json3='{"date":{"$gte":"$date(2016-05-19)","$lte":"$date(2016-05-25)"},"location":{"$in":["Seoul"]},"source":"IGOBUSIMFGOV"}'
+  print(4)
+  url=paste0("http://10.176.148.60:5000/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query=",json3)
+  #url2=paste0("http://10.176.148.60:5000/api/data?api_key=CD75737EF4CAC292EE17B85AAE4B6&query=",json3)
+  
+  print(url)
+  
+  #jsontest='{"9":{"33":{"74":[{"label":"V155","labs":"Bird Flu and Epidemics"},{"label":"V415","labs":"Fowl and Meat Industry"}]}}}'
+      
+#  dt=jsonlite::fromJSON(jsontest)
+  mydata=jsonlite::fromJSON(url)
+  #mydata=getURL(url)
+  print(mydata)
+  #test=mydata$data
+  #date=mydata$data$date
+  #location=mydata$data$location
+  #country=mydata$data$country
+  
+  #location2=unique(location)
+  #datesort=sort(date)
+  #maxdate=datesort[which.max(datesort)]
+  #mindate=datesort[which.min(datesort)]
+  
+  #outdata=list(maxdate=maxdate,mindate=mindate,location=location2)
+  jsontest=rjson::toJSON(mydata)
+  
+  result=list(jsontest)
+  }
+  response$write(result)
+  #print(response)
+  response$finish()
+}
