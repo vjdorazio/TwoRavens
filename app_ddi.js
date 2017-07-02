14817186c17b9178d899e20f0a8c0af669c35ab5
Kripanshu Bhargava <kripanshubhargava@gmail.com>
2017-07-02 06:02:44 -0500

    Scatter to heatmap added

--- app_ddi.js
+++ app_ddi.js
@@ -659,6 +659,337 @@ readPreprocess(url=pURL, p=preprocess, v=null, callback=function(){
                });
 
 
+function bivariatePlot(x_axis,y_axis)
+{
+   /* for(var i=0; i<1000;i++)
+    {
+
+        console.log("x_axis is :"+ x_axis[i]);
+        console.log("y_axis is :"+ y_axis[i]);
+    }
+    */
+
+    var numPoints = 70,
+        size=400,
+        size1 = 100,
+        numRows = 16,
+        numCols = 16,
+        showingScatter = true,
+        scatterDirty = false,
+        data = null,
+        cells = null,
+        color = d3.interpolateRgb("#fff", "#063");
+   // var NA=-1;
+   // var x_axis=[1,15,12,32,424,42,6,5,34,29,982,87,67,56,43,44,5,6,666,NA,66];
+   // var y_axis=[4,65,12,32,44,81,61,51,314,219,82,7,37,56,73,14,52,9,66,NA,616];
+
+    var getEmptyCells = function() {
+        var emptyCells = [];
+        for (var rowNum = 0; rowNum < numRows; rowNum++) {
+            emptyCells.push([]);
+            var row = emptyCells[emptyCells.length - 1];
+            for (var colNum = 0; colNum < numCols; colNum++) {
+                row.push({
+                    row: rowNum,
+                    col: colNum,
+                    density: 0,
+                    points: []
+                });
+            }
+        }
+        return emptyCells;
+    };
+
+    var clearCells = function() {
+        for (var rowNum = 0; rowNum < numRows; rowNum++) {
+            for (var colNum = 0; colNum < numCols; colNum++) {
+                cells[rowNum][colNum].density = 0;
+                cells[rowNum][colNum].points = [];
+            }
+        }
+    };
+
+    var randomizeData = function() {
+        data = [];
+
+        if (cells === null) {
+            cells = getEmptyCells();
+        }
+        else {
+            clearCells();
+        }
+
+        var x, y, col, row,x1,y1;
+        for (var i = 0; i < x_axis.length; i++) {
+            if (isNaN(x_axis[i]) || isNaN(y_axis[i])) {
+                console.log("NA is at : "+i)
+            } else {
+                // x = Math.random() *size1;
+                //y = Math.random() *size1;
+                x1 = x_axis[i];
+                y1 = y_axis[i];
+                col = Math.min(Math.floor(x1 / size1 * numCols), numCols - 1);
+                row = Math.min(Math.floor(y1 / size1 * numRows), numRows - 1);
+
+                data.push({
+                    x: x1,
+                    y: y1,
+                    col: col,
+                    row: row,
+                    cell: cells[row][col],
+                    ind: i
+
+
+                });
+               // console.log("for the i: " + i + " x : " + x1 + " y: " + y1);
+              //  console.log("for the i: " + i + " col : " + col + " row: " + row);
+                cells[row][col].points.push(data[data.length - 1]);
+            }
+        }
+    };
+
+    var selectPoints = function(points) {
+        d3.selectAll(points).attr("r", 4).attr("stroke", "#f00").attr("stroke-width", 3);
+
+        for (var i = 0; i < points.length; i++) {
+            points[i].parentNode.appendChild(points[i]);
+        }
+    };
+
+    var deselectPoints = function(points) {
+        d3.selectAll(points).attr("r", 2).attr("stroke", "none");
+    };
+
+    var selectCell = function(cell) {
+        d3.select(cell).attr("stroke", "#f00").attr("stroke-width", 3);
+
+        cell.parentNode.parentNode.appendChild(cell.parentNode);
+        cell.parentNode.appendChild(cell);
+    };
+
+    var deselectCell = function(cell) {
+        d3.select(cell).attr("stroke", "#fff").attr("stroke-width", 1);
+    };
+
+    var onPointOver = function(point, data) {
+        selectPoints([point]);
+        var cell = d3.select("div#heatchart").select('[cell="r' + data.row + 'c' + data.col + '"]');
+        selectCell(cell.node());
+    };
+
+    var onPointOut = function(point, data) {
+        deselectPoints([point]);
+        var cell = d3.select("div#heatchart").select('[cell="r' + data.row + 'c' + data.col + '"]');
+        deselectCell(cell.node());
+    };
+
+    var createScatterplot = function() {
+        var scatterplot = d3.select("div#scatterplot").append("svg:svg").attr("width", size).attr("height", size);
+
+        scatterplot.selectAll("circle").data(data).enter().append("svg:circle").attr("cx", function(d, i) {
+            return d.x;
+        }).attr("cy", function(d, i) {
+            return d.y;
+        }).attr("r", size/100).attr("ind", function(d) {
+            return d.ind;
+        }).on("mouseover", function(d) {
+            onPointOver(this, d);
+        }).on("mouseout", function(d) {
+            onPointOut(this, d);
+        });
+
+
+    };
+
+    d3.select("scatterplot").append("text"," We have NA values in the data.");
+    var onCellOver = function(cell, data) {
+        selectCell(cell);
+
+        if (showingScatter) {
+            var pointEls = [];
+
+            for (var i = 0; i < data.points.length; i++) {
+                pointEls.push(d3.select("div#scatterplot").select('[ind="' + data.points[i].ind + '"]').node());
+            }
+
+            selectPoints(pointEls);
+        }
+    };
+
+    var onCellOut = function(cell, data) {
+        deselectCell(cell);
+
+        if (showingScatter) {
+            var pointEls = [];
+
+            for (var i = 0; i < data.points.length; i++) {
+                pointEls.push(d3.select("div#scatterplot").select('[ind="' + data.points[i].ind + '"]').node());
+            }
+
+            deselectPoints(pointEls);
+        }
+    };
+
+    var updateScatterplot = function() {
+        // select
+        var dots = d3.select("div#scatterplot").select("svg").selectAll("circle").data(data);
+
+        // enter
+        dots.enter().append("svg:circle").attr("cx", function(d, i) {
+            return d.x;
+        }).attr("cy", function(d, i) {
+            return d.y;
+        }).attr("r", 2).attr("ind", function(d) {
+            return d.ind;
+        }).on("mouseover", function(d) {
+            onPointOver(this, d);
+        }).on("mouseout", function(d) {
+            onPointOut(this, d);
+        });
+
+        // update
+        dots.attr("cx", function(d, i) {
+            return d.x;
+        }).attr("cy", function(d, i) {
+            return d.y;
+        }).attr("ind", function(d) {
+            return d.ind;
+        }).on("mouseover", function(d) {
+            onPointOver(this, d);
+        }).on("mouseout", function(d) {
+            onPointOut(this, d);
+        });
+
+        // exit
+        dots.exit().remove();
+    };
+
+    var createHeatchart = function() {
+        var min = 999;
+        var max = -999;
+        var l;
+
+        for (var rowNum = 0; rowNum < cells.length; rowNum++) {
+            for (var colNum = 0; colNum < numCols; colNum++) {
+                l = cells[rowNum][colNum].points.length;
+
+                if (l > max) {
+                    max = l;
+                }
+                if (l < min) {
+                    min = l;
+                }
+            }
+        }
+
+        var heatchart = d3.select("div#heatchart").append("svg:svg").attr("width", size).attr("height", size);
+
+        heatchart.selectAll("g").data(cells).enter().append("svg:g").selectAll("rect").data(function(d) {
+            return d;
+        }).enter().append("svg:rect").attr("x", function(d, i) {
+            return d.col * (size / numCols);
+        }).attr("y", function(d, i) {
+            return d.row * (size / numRows);
+        }).attr("width", size / numCols).attr("height", size / numRows).attr("fill", function(d, i) {
+            return color((d.points.length - min) / (max - min));
+        }).attr("stroke", "#fff").attr("cell", function(d) {
+            return "r" + d.row + "c" + d.col;
+        }).on("mouseover", function(d) {
+            onCellOver(this, d);
+        }).on("mouseout", function(d) {
+            onCellOut(this, d);
+        });
+    };
+
+    var updateHeatchart = function() {
+        var min = 999;
+        var max = -999;
+        var l;
+
+        for (var rowNum = 0; rowNum < cells.length; rowNum++) {
+            for (var colNum = 0; colNum < numCols; colNum++) {
+                l = cells[rowNum][colNum].points.length;
+
+                if (l > max) {
+                    max = l;
+                }
+                if (l < min) {
+                    min = l;
+                }
+            }
+        }
+
+        d3.select("div#heatchart").select("svg").selectAll("g").data(cells).selectAll("rect").data(function(d) {
+            return d;
+        }).attr("x", function(d, i) {
+            return d.col * (size / numCols);
+        }).attr("y", function(d, i) {
+            return d.row * (size / numRows);
+        }).attr("fill", function(d, i) {
+            return color((d.points.length - min) / (max - min));
+        }).attr("cell", function(d) {
+            return "r" + d.row + "c" + d.col;
+        }).on("mouseover", function(d) {
+            onCellOver(this, d);
+        }).on("mouseout", function(d) {
+            onCellOut(this, d);
+        });
+    };
+
+    var onRandomizeClick = function() {
+        randomizeData();
+
+        if (showingScatter) {
+            updateScatterplot();
+        }
+        else {
+            scatterDirty = true;
+        }
+
+        updateHeatchart();
+    };
+
+    var onNumPointsChange = function(event) {
+        numPoints = event.target.options[event.target.selectedIndex].value;
+        randomizeData();
+
+        if (showingScatter) {
+            updateScatterplot();
+        }
+        else {
+            scatterDirty = true;
+        }
+
+        updateHeatchart();
+    };
+
+    var onShowScatterplotChange = function(event) {
+        showingScatter = event.target.checked;
+
+        if (showingScatter) {
+            if (scatterDirty) {
+                updateScatterplot();
+                scatterDirty = false;
+            }
+
+            d3.select("div#scatterplot").select("svg").attr("visibility", "visible");
+        }
+        else {
+            d3.select("div#scatterplot").select("svg").attr("visibility", "hidden");
+        }
+    };
+
+    var init = function() {
+        randomizeData();
+        createScatterplot();
+        createHeatchart();
+    };
+
+    init();
+
+
+}
+
 ////////////////////////////////////////////
 // everything below this point is a function
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -2538,9 +2869,13 @@ function estimate(btn) {
         d3.select("#result_right")
             .style("display", "block");
 
-        d3.select("#resultsView")
+       /* d3.select("#resultsView")
         .style("display", "block");
-
+        */
+        d3.select("#scatterplot")
+            .style("display", "block");
+        d3.select("#heatchart")
+            .style("display", "block");
         d3.select("#modelView")
         .style("display", "block");
 
@@ -2668,9 +3003,14 @@ function explore(btn) {
 
         d3.select("#result_right")
             .style("display", "block");
+      /*
         d3.select("#resultsView")
         .style("display", "block");
-
+        */
+        d3.select("#scatterplot")
+            .style("display", "block");
+        d3.select("#heatchart")
+            .style("display", "block");
         d3.select("#modelView_Container")
         .style("display", "block");
 
@@ -2977,7 +3317,7 @@ function viz_explore(m,json_vizexplore,model_name_set) {
 
     var get_data=[];
     get_data=model_name_set.split("-");
-   // console.log("asdasdsdisudwiudiquwndiuawndiuawndiwudnqiuwdnaiuwndiuawndiuawndiuwnadiwd");
+
   //  console.log(get_data[0]+" and "+get_data[1]);
 
     var model_name1=get_data[0]+"-"+get_data[1];
@@ -2992,8 +3332,7 @@ var model_name2=get_data[1]+"-"+get_data[0];
         }
     }
 
-    var myparent = document.getElementById("resultsView");
-    removeKids(myparent);
+
 
     var json = json_vizexplore;
     //   console.log("json explore viz:"+json.toString());
@@ -3011,10 +3350,50 @@ var model_name2=get_data[1]+"-"+get_data[0];
 
 
     // image added to the div
+
+
+    var x_axis=[];
+    var y_axis=[];
     for (var i in json.plotdata) {
-for(var j in json.plotdata[i]) {
-    console.log(" the myxlab is : " + j);
+
+for(var j in json.plotdata[i].varname) {
+
+    console.log(" the var name is : " + json.plotdata[i].varname[j]);
+
+    if(json.plotdata[i].varname[j]=== get_data[0]){
+        for (var k in json.plotdata[i].data) {
+
+
+           // console.log(json.plotdata[i].varname[j] + "data is : " + json.plotdata[i].data[k]);
+x_axis[k]=json.plotdata[i].data[k];
+
+        }
+    }
+    if(json.plotdata[i].varname[j]===get_data[1])
+    {
+        for (var k in json.plotdata[i].data) {
+
+
+           // console.log(json.plotdata[i].varname[j] + "data is : " + json.plotdata[i].data[k]);
+y_axis[k]=json.plotdata[i].data[k];
+
+        }
+    }
+
 }
+}
+//console.log(" the x length is : "+ x_axis.length);
+  //  console.log(" the x length is : "+ y_axis.length);
+bivariatePlot(x_axis,y_axis);
+/*
+for(var t=0; t<x_axis.length; t++){
+        console.log(" the x axis is :"+x_axis[t] );
+    console.log(" ");
+    console.log(" the y axis is :"+y_axis[t] );
+}
+
+*/
+
 /*
 if(i==model_name_set) {
     var zfig = document.createElement("img");
@@ -3029,7 +3408,7 @@ if(i==model_name_set) {
     d3.select("#resultsView").style("box-shadow","1px 1px 3px grey").style("background-color","#F5F5F5");
 */
 
-    }
+
 
     var table_obj=[];
     var colnames=[];
