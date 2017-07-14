295023eacf4ca19d7e91fb2d1cbd5ff4571b0133
Shoeboxam <shoeboxam@gmail.com>
2017-07-13 19:18:11 -0500

    Experimental: embed subset data in existing tree hierarchy

--- app_ddi_eventdata_query.js
+++ app_ddi_eventdata_query.js
@@ -1,26 +1,21 @@
 // Right panel of subsetting menu
 
-// addGroup(document.getElementById('queryTree'), false);
 
-var logDropdown =  '<div class="dropdown" style="display:inline;float:left"><button class="btn btn-primary dropdown-toggle btn-xs" type="button" data-toggle="dropdown"> and <span class="caret"></span></button>';
-logDropdown += '<ul class="dropdown-menu dropdown-menu-right" id="addDropmenu" style="float:left;margin:0;padding:0;width:45px;min-width:45px">' +
-    '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="1">and </a></li>' +
-    '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="2">or </a></li>' +
-    '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="1">nand </a></li>' +
-    '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="2">nor </a></li>' +
-    '</ul></div>';
-
-var cancelButton = "<button type='button' class='btn btn-primary btn-xs' style='background:none;border:none;box-shadow:none;float:right;margin-top:3px' onclick='this.parentElement.remove()'><span class='glyphicon glyphicon-remove' style='color:#ADADAD'></span></button></div>";
+// This is the data format for
+    // {
+    //     name: 'Root',
+    //     operation: 'and',        // If exists, have a logical drop down
+    //     children: [],            // If exists, have a triangle expander
+    //     negate: false            // If exists, have a negation button
+    //     cancellable: false       // If exists and false, disable the delete button
+    // }
 
 var data = [
     {
         name: 'Root',
-        group: true,
         operation: 'and',
-        children: [
-            { name: 'Location Subset' },
-            { name: 'Date Subset' }
-        ]
+        children: [],
+        cancellable: false
     }
 ];
 
@@ -30,7 +25,7 @@ function togglebutton(id){
 }
 
 function logicbutton(id){
-    logDropdown =  '<div class="dropdown" style="display:inline;float:left"><button class="btn btn-primary dropdown-toggle btn-xs" type="button" data-toggle="dropdown"> and <span class="caret"></span></button>';
+    logDropdown =  ' <div class="dropdown" style="display:inline;"><button class="btn btn-primary dropdown-toggle btn-xs" type="button" data-toggle="dropdown"> and <span class="caret"></span></button>';
     logDropdown += '<ul class="dropdown-menu dropdown-menu-right" id="addDropmenu" style="float:left;margin:0;padding:0;width:45px;min-width:45px">' +
         '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="1">and </a></li>' +
         '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="2">or </a></li>' +
@@ -40,6 +35,9 @@ function logicbutton(id){
     return logDropdown
 }
 
+function cancelButton(id){
+    return "<button type='button' class='btn btn-primary btn-xs' style='background:none;border:none;box-shadow:none;float:right;margin-top:3px' onclick='this.parentElement.remove()'><span class='glyphicon glyphicon-remove' style='color:#ADADAD'></span></button></div>";
+}
 
 $(function() {
     $('#queryTree').tree({
@@ -50,59 +48,39 @@ $(function() {
         onCreateLi: function(node, $li) {
             // Append a link to the jqtree-element div.
             // The link has an url '#node-[id]' and a data property 'node-id'.
-
-            $li.find('.jqtree-element').append(togglebutton(node.id));
+            if ('operation' in node) {
+                $li.find('.jqtree-element').append(logicbutton(node.id));
+            }
+            if ('negate' in node) {
+                $li.find('.jqtree-element').append(togglebutton(node.id));
+            }
+            if (!('cancellable' in node) || (node['cancellable'] === true)) {
+                $li.find('.jqtree-element').append(cancelButton(node.id));
+            }
         }
     });
 });
 
-
-$('#queryTree').bind(
-    'tree.click',
-    function(event) {
-        // The clicked node is 'event.node'
-        var node = event.node;
-        alert(node.name);
+$('#queryTree').on(
+    'tree.move',
+    function(event)
+    {
+        event.preventDefault();
+        event.move_info.do_move();
+        // $.post('your_url', {tree: $(this).tree('toJson')});
     }
 );
 
+function addGroup(parent){
 
-a ="<button id='boolToggle' type='button' class='btn btn-primary btn-xs active' data-toggle='button' aria-pressed='true'>not</button> "
-function addGroup(parent, cancellable=true){
-    var container = "<div id='group' class='container' style='float:left;text-align:left;display:inline-block;background:rgba(90%,50%,20%,0.1);margin-top:3px;padding:3px 0 3px 13px;width:100%;'>"
-
-    // var logToggle = "<button id='logToggle' type='button' class='btn btn-primary btn-xs active' onclick=changeLogical(this) data-toggle='button' aria-pressed='true' style='width:35px;'>and</button> ";
-
-    var cancelButton = "<button type='button' class='btn btn-primary btn-xs' style='";
-    if (!cancellable){
-        cancelButton += 'visibility:hidden;';
-    }
-    cancelButton += "background:none !important;border:none;box-shadow:none;float:right;margin-top:3px' onclick='this.parentElement.remove()'><span class='glyphicon glyphicon-remove' style='color:#ADADAD'></span></button>";
-
-    var logDropdown =  '<div class="dropdown" style="display:inline;float:left"><button class="btn btn-primary dropdown-toggle btn-xs" type="button" data-toggle="dropdown"> and <span class="caret"></span></button>';
-    logDropdown += '<ul class="dropdown-menu dropdown-menu-right" id="addDropmenu" style="float:left;margin:0;padding:0;width:45px;min-width:45px">' +
-        '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="1">and </a></li>' +
-        '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="2">or </a></li>' +
-        '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="1">nand </a></li>' +
-        '<li style="margin:0;padding:0;width:45px"><a style="margin:0;height:20px;padding:2px;width:43px!important" data-addsel="2">nor </a></li>' +
-        '</ul></div>';
-
-    var addRuleButton = "<button type='button' class='btn btn-primary btn-xs' onclick='addRule(this.parentElement)' style='float:right;display:inline-block;'>Add Rule</button> ";
-    var addGroupButton = "<button type='button' class='btn btn-primary btn-xs' onclick='addGroup(this.parentElement)' style='float:right;display:inline-block;'>Add Group</button>";
-
-    parent.innerHTML += container + logDropdown + cancelButton + addGroupButton + addRuleButton + "</div>";
 }
-
-// When a rule is added:
-// 1. add the tag 'data-subset=<subsetCount>' to the new rule div, and increment data-subsets
-// 2. add a dictionary to subsetDicts that contains the current subset preferences
-
-var subsetCount = 0;
-var subsetDicts = [];
 
 function addRule(){
     // Index zero is root node. Add subset pref to nodes
-    data[0]['children'].push(getSubsetPreferences())
+    if (subsetSelection !== "") {
+        data[0]['children'].push(getSubsetPreferences());
+        $('#queryTree').tree('loadData', data);
+    }
 }
 
 /**
@@ -111,9 +89,24 @@ function addRule(){
  */
 function getSubsetPreferences(){
     if (subsetSelection == 'Date') {
+        var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
+            "July", "Aug", "Sep", "Oct", "Nov", "Dec"
+        ];
         return {
-            fromDate: dateminUser,
-            toDate: datemaxUser
+            name: 'Date Subset',
+            operation: 'and',
+            children: [
+                {
+                    name: 'From: ' + monthNames[dateminUser.getMonth()] + ' ' + String(dateminUser.getFullYear()),
+                    negate: false,
+                    fromDate: dateminUser
+                },
+                {
+                    name: 'To:   ' + monthNames[datemaxUser.getMonth()] + ' ' + String(datemaxUser.getFullYear()),
+                    negate: false,
+                    toDate: datemaxUser
+                }
+            ]
         };
     }
     if (subsetSelection == 'Location'){
