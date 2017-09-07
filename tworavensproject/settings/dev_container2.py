428ee3454bfec364975be6be5509f783bf663ebe
Raman Prasad <raman_prasad@harvard.edu>
2017-09-07 16:10:09 -0400

    settings update

---
+++ tworavensproject/settings/dev_container2.py
@@ -0,0 +1,3 @@
+from local_settings import *
+
+R_DEV_SERVER_BASE = 'http://0.0.0.0:8000/custom/'
--- tworavensproject/settings/local_settings.py
+++ tworavensproject/settings/local_settings.py
@@ -49,8 +49,8 @@ TIME_ZONE = 'America/New_York'
 
 # TwoRavens R service test
 
-#R_DEV_SERVER_BASE = 'http://0.0.0.0:8000/custom/'
-R_DEV_SERVER_BASE = 'http://0.0.0.0:8060/custom/'
+R_DEV_SERVER_BASE = 'http://0.0.0.0:8000/custom/'
+#R_DEV_SERVER_BASE = 'http://0.0.0.0:8060/custom/'
 
 RECORD_R_SERVICE_ROUTING = True # log R service requests/response JSON to db
 
