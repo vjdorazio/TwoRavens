24e999203f20efb7595e06d53a95542ab604dcad
Raman Prasad <raman_prasad@harvard.edu>
2017-09-07 22:41:00 -0400

    docker note

--- tworaven_apps/test_data/docker_util.py
+++ tworaven_apps/test_data/docker_util.py
@@ -4,6 +4,7 @@ shortcut to run docker remove commands
 
 def get_rmi_command(info_list):
     """
+    JUST USE: docker rmi $(docker images -f "dangling=true" -q)
     <none>              <none>              591446e60492        4 days ago          796MB
     <none>              <none>              f2f37f4e35fa        4 days ago          1.02GB
     <none>              <none>              ff14b547855c        4 days ago          1.02GB
@@ -19,6 +20,8 @@ def get_rmi_command(info_list):
 
 def get_rm_command(info_list):
     """
+    JUST USE: docker rm $(docker ps -q -f status=exited)
+
     CONTAINER ID        IMAGE               COMMAND                  CREATED             STATUS                    PORTS               NAMES
     9094581ecdcf        9721cecf623c        "/bin/sh -c '/bin/..."   4 days ago          Exited (126) 4 days ago                       frosty_colden
     7e3c9c8a6e4d        591446e60492        "/bin/sh -c '/bin/..."   4 days ago          Exited (1) 4 days ago                         gallant_pare
