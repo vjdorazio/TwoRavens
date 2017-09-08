4534857e6baec8aa347aaa78dfec678e440410e1
Raman Prasad <raman_prasad@harvard.edu>
2017-09-07 21:50:37 -0400

    settings to use /assets/dist instead of /assets/build

--- tworavensproject/settings/dev_container2.py
+++ tworavensproject/settings/dev_container2.py
@@ -4,3 +4,9 @@ import os
 
 R_DEV_SERVER_BASE = os.environ.get('R_DEV_SERVER_BASE',
                                    'http://0.0.0.0:8000/custom/')
+
+
+WEBPACK_LOADER['DEFAULT'].update(\
+    dict(BUNDLE_DIR_NAME='dist/',
+         STATS_FILE=join(BASE_DIR, 'webpack-stats-prod.json'))\
+    )
